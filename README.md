c5dd28bcbd5c54d01740b4628ecc342e68af2287
KristofaJosh <christopherjoshua18@gmail.com>
2020-03-01 10:06:55 +0100

    fix login error code

--- README.md
+++ README.md
@@ -1,68 +1,23 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+# Home Assignment: Decagon Central
+DecagonCentral is a database of all decadevs and staffs at decagon.
 
-## Available Scripts
+User story:
+* As a user I want to be able to signup
+* As a user I want to be able to login
+* As a user I should be able to see my dashboard
 
-In the project directory, you can run:
+Implement the following screens using react and implement the api endpoints to make it functional end to end:
 
-### `yarn start`
+* https://xd.adobe.com/spec/6e5673b7-4d64-4cbd-7b8c-66273a119090-1867/
+* https://xd.adobe.com/spec/6e5673b7-4d64-4cbd-7b8c-66273a119090-1867/screen/c646aa50-8b70-4660-aa25-937bdf65a705/Web-1920-1
+* https://xd.adobe.com/spec/6e5673b7-4d64-4cbd-7b8c-66273a119090-1867/screen/9cf998d0-ff67-4c82-aa00-4da3ec997272/Web-1920-2
 
-Runs the app in the development mode.<br />
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 
-The page will reload if you make edits.<br />
-You will also see any lint errors in the console.
+You can use any database of your choice, SQL/NOSQL.
+Make sure to write tests for all your endpoints and all your components. Make sure your folder structure is correct. You may refer to https://github.com/decadevs/bouncer for clarity.
 
-### `yarn test`
+Here is a resource to help out with testing:
+https://dev.to/itnext/how-you-can-use-react-testing-library-to-test-component-surface-49pm
 
-Launches the test runner in the interactive watch mode.<br />
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
 
-### `yarn build`
 
-Builds the app for production to the `build` folder.<br />
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.<br />
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `yarn eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (Webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/code-splitting
-
-### Analyzing the Bundle Size
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size
-
-### Making a Progressive Web App
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app
-
-### Advanced Configuration
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/advanced-configuration
-
-### Deployment
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
-
-### `yarn build` fails to minify
-
-This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,97 +1,109 @@
-import React, {useState} from 'react'
-import Button from '../Button/Button'
-import TextLink from '../TextLink/TextLink'
-import './login.css'
-import Main from '../../Features/Main/Main';
-import Bkg from '../../Features/BackgroundSidebar/BackgroundSidebar'
-import axios from 'axios';
+import React, { useState } from "react";
+import Button from "../Button/Button";
+import TextLink from "../TextLink/TextLink";
+import "./login.css";
+import Main from "../../Features/Main/Main";
+import Bkg from "../../Features/BackgroundSidebar/BackgroundSidebar";
+import axios from "axios";
 
 export default function Login(props) {
+  const [logData, setLogData] = useState({ email: "", password: "" });
+  const [error, setError] = useState([
+    "",
+    "We'll never share your email with anyone else."
+  ]);
+  const [token, setToken] = useState("");
 
-    
-    const [logData, setLogData] = useState({email:'', password:''});
-    const [error, setError] = useState('We\'ll never share your email with anyone else.');
-    const [token, setToken] = useState('');
+  if (localStorage.token_key) {
+    props.history.push("/");
+  } else {
+    localStorage.setItem("token_key", token);
+    // console.log(token)
+  }
 
+  const formData = e => {
+    setLogData({ ...logData, [e.target.name]: e.target.value });
+  };
 
-    if(localStorage.token_key){
-        props.history.push('/')
-    }else{
-        localStorage.setItem('token_key', token)
-        // console.log(token)
+  const config = {
+    headers: {
+      "Content-type": "application/json"
     }
+  };
 
+  //send to api route
+  const subForm = () => {
+    // http://127.0.0.1:8000/auth/token/login/
 
-    const formData = (e) => {
-        setLogData({...logData,[e.target.name]: e.target.value})
+    // returns token
+
+    // console.log(logData)
+
+    localStorage.registered = "";
+
+    if (logData.username !== "" && logData.password !== "") {
+      axios
+        .post("http://127.0.0.1:8000/auth/login/", logData, config)
+        .then(res => {
+          // console.log(res.data)
+          setToken(res.data);
+          //goto dashboard
+          props.history.push("/");
+        })
+        .catch(err => {
+          // console.log(err.response)
+          if (err.response.status === 400) {
+            setError(["error", "Wrong Username or Password"]);
+          } else {
+            setError(["error", "Enter your email address"]);
+          }
+        });
+    } else {
+      setError(["error", "Enter your email address and/or password"]);
     }
+  };
 
-    const config = {     
-        headers: { 
-            'Content-type': 'application/json', 
-        }
-    }
+  return (
+    <>
+      <Bkg />
+      <Main width={65}>
+        <div style={{ color: "green" }}>{localStorage.registered || ""}</div>
+        <br />
+        <form onSubmit={subForm}>
+          <h4>Login</h4>
+          <div className="form-group">
+            <label htmlFor="exampleInputEmail1">Email:</label>
+            <input
+              type="email"
+              name="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              onChange={formData}
+              required
+            ></input>
+          </div>
+          <div className="form-group">
+            <label htmlFor="exampleInputPassword1">Password:</label>
+            <input
+              type="password"
+              name="password"
+              className="form-control"
+              id="exampleInputPassword1"
+              onChange={formData}
+              required
+            ></input>
+          </div>
+          <small id="emailHelp" className="form-text text-muted">
+            <span className={error[0]}>{error[1]}</span>
+          </small>
 
-    //send to api route
-    const subForm = () => {
-
-        // http://127.0.0.1:8000/auth/token/login/
-
-        // returns token
-
-        // console.log(logData)
-
-
-
-        localStorage.registered = '';
-
-        if (logData.username !== '' && logData.password !== ''){
-            axios.post('http://127.0.0.1:8000/auth/login/', logData, config)
-                .then(res => {
-                    console.log(res.data)
-                 setToken(res.data)
-                //goto dashboard
-                props.history.push('/')
-
-            })
-            .catch(err => {
-                // console.log(err.response)
-                if(err.response.status === 400){
-                    setError('Wrong Username or Password')
-                }else{
-                    setError('Enter your email address')
-                }
-            })
-        }else{
-            setError('Enter your email address and/or password')
-        }
-    }
-
-
-    return (
-        <>
-        <Bkg />
-        <Main width={65}>
-        <div style={{color:'green'}}>{localStorage.registered || ''}</div>
-        <br/>
-        <form onSubmit = {subForm}>
-            <h4>Login</h4>
-            <div className="form-group">
-                <label htmlFor="exampleInputEmail1">Email:</label>
-                <input type="email" name='email' className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"  onChange={formData} required></input>
-            </div>
-            <div className="form-group">
-                <label htmlFor="exampleInputPassword1">Password:</label>
-                <input type="password" name='password' className="form-control" id="exampleInputPassword1" onChange={formData} required></input>
-            </div>
-            <small id="emailHelp" className="form-text text-muted">{error}</small>
-
-            <div className="submit-section">
-                <Button title='Login' fnc={subForm} />
-                <TextLink title='Create an account' LinkTo='/register' />
-            </div>
+          <div className="submit-section">
+            <Button title="Login" fnc={subForm} />
+            <TextLink title="Create an account" LinkTo="/register" />
+          </div>
         </form>
-        </Main>
-        </>
-    )
+      </Main>
+    </>
+  );
 }
