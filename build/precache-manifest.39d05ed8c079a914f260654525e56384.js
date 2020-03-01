e13bc696fc48320d760bb42ba9fa00df7ad22c0a
KristofaJosh <christopherjoshua18@gmail.com>
2020-03-01 11:07:21 +0100

    upload production code

---
+++ build/precache-manifest.39d05ed8c079a914f260654525e56384.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb641a11c68ab062f85635cd0609c813",
+    "url": "/index.html"
+  },
+  {
+    "revision": "303b875c2cc0ca6a4ce2",
+    "url": "/static/css/main.0f885edd.chunk.css"
+  },
+  {
+    "revision": "e33382df27a4690bda00",
+    "url": "/static/js/2.d5aacb53.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.d5aacb53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "303b875c2cc0ca6a4ce2",
+    "url": "/static/js/main.b853bcda.chunk.js"
+  },
+  {
+    "revision": "a8139511e7e3c6f8467f",
+    "url": "/static/js/runtime-main.1c2d59f4.js"
+  },
+  {
+    "revision": "8ba062c10fe553ebf996e95f8f09a47b",
+    "url": "/static/media/background_image.8ba062c1.png"
+  },
+  {
+    "revision": "634ed344c55d8799fcf3a30b178bb1e2",
+    "url": "/static/media/decagon-logo.634ed344.png"
+  },
+  {
+    "revision": "846d42f8e72142b1d887ad51f942bf98",
+    "url": "/static/media/image1.846d42f8.png"
+  },
+  {
+    "revision": "1534ff73cb2f4f4c58a927aa0b9d213c",
+    "url": "/static/media/image2.1534ff73.png"
+  },
+  {
+    "revision": "bfc761e1f12a42d127d20b8fae2aab77",
+    "url": "/static/media/image3.bfc761e1.png"
+  },
+  {
+    "revision": "67d43f58d8e0a167821991ea7b02c401",
+    "url": "/static/media/image5.67d43f58.png"
+  },
+  {
+    "revision": "cc11e5eea51256f5274bf5d43991eaba",
+    "url": "/static/media/outline.cc11e5ee.svg"
+  }
+]);
